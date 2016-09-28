e82f12fc0ea159c365a7bcf71b0ef96edfefa787
kickinbahk <kickinbahk@gmail.com>
2016-09-28 14:58:04 -0700

    update alert to contain value of slider

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,27 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var currentValue: Int = 0
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  @IBAction func showAlert() {
+    let message = "The value of the slider is: \(currentValue)"
+    let alert = UIAlertController(title: "Hello, World",
+                                  message: message,
+                                  preferredStyle: .alert)
+    let action = UIAlertAction(title: "OK",
+                               style: .default,
+                               handler: nil)
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+  }
 
-    @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World",
-                                      message: "This is my next app",
-                                      preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome",
-                                   style: .default,
-                                   handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-
+  @IBAction func sliderMoved(_ slider: UISlider) {
+    currentValue = lroundf(slider.value)
+  }
 }
 
