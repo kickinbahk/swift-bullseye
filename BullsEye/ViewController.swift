f5c17c0bcb11e64e80bc0fe2fa24e9ae874dffab
kickinbahk <kickinbahk@gmail.com>
2016-09-29 11:20:59 -0700

    move initializer to new method

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,11 +11,11 @@ import UIKit
 class ViewController: UIViewController {
   @IBOutlet weak var slider: UISlider!
   var currentValue: Int = 0
+  var targetValue: Int = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    currentValue = lroundf(slider.value)
-    // Do any additional setup after loading the view, typically from a nib.
+    startNewRound()
   }
 
   override func didReceiveMemoryWarning() {
@@ -24,7 +24,8 @@ class ViewController: UIViewController {
   }
 
   @IBAction func showAlert() {
-    let message = "The value of the slider is: \(currentValue)"
+    let message = "The value of the slider is: \(currentValue)" +
+                  "\nThe target value is: \(targetValue)"
     let alert = UIAlertController(title: "Hello, World",
                                   message: message,
                                   preferredStyle: .alert)
@@ -33,10 +34,17 @@ class ViewController: UIViewController {
                                handler: nil)
     alert.addAction(action)
     present(alert, animated: true, completion: nil)
+    startNewRound()
   }
 
   @IBAction func sliderMoved(_ slider: UISlider) {
     currentValue = lroundf(slider.value)
+  }
+  
+  func startNewRound() {
+    targetValue = 1 + Int(arc4random_uniform(100))
+    currentValue = 50
+    slider.value = Float(currentValue)
   }
 }
 
