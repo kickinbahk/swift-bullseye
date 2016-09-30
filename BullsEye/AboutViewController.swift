5265bdc8829138ba379c9aadf123e8ae254c6eac
kickinbahk <kickinbahk@gmail.com>
2016-09-30 09:25:14 -0700

    add webview to display html page for about, change close button

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -11,19 +11,25 @@ import UIKit
 class AboutViewController: UIViewController {
 
   override func viewDidLoad() {
-      super.viewDidLoad()
-
+    super.viewDidLoad()
+    if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+      if let htmlData = try? Data(contentsOf: url) {
+        let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+        webView.load(htmlData, mimeType: "text/html",
+                     textEncodingName: "UTF-8", baseURL: baseURL)
+      }
+    }
       // Do any additional setup after loading the view.
   }
 
-  override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-  }
+  @IBOutlet weak var webView: UIWebView!
   
   @IBAction func close() {
     dismiss(animated: true, completion: nil)
   }
   
-
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 }
